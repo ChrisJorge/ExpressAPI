342f078a8a5d014666f861e00faba99dd9e5f4e6
Chris <Christopherjorge47@icloud.com>
2024-04-15 15:01:07 -0400

    Created desserts array and desserts file

---
+++ data/desserts.js
@@ -0,0 +1,25 @@
+const desserts = [
+    {
+        name: 'Ice Cream',
+        cost: '6.99',
+        flavors: ['Strawberry', 'Cherry', 'Vanilla', 'Chocolate', 'Cookies and Cream', 'Chocolate Chip', 'Mint']
+    },
+    {
+        name: 'Fried Oreos',
+        cost: '5.99'
+    },
+    {
+        name: 'Jello',
+        cost: '2.99',
+        flavors: ['Strawberry', 'Cherry', 'Lime', 'Orange']
+    },
+    {
+        name: 'Funnel Cake',
+        cost: '9.99'
+    },
+    {
+        name: 'Pie',
+        cost: '7.99',
+        flavors: ['Strawberry', 'Cherry', 'Apple', 'Pumpkin']
+    }
+]
