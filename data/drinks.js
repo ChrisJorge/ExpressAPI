0ef7789cbc9832323649408fc414f08e09e636c0
Chris <Christopherjorge47@icloud.com>
2024-04-15 14:54:29 -0400

    Created drinks array and drinks js file

---
+++ data/drinks.js
@@ -0,0 +1,43 @@
+const drinks = [
+    {
+        name: 'Iced Tea',
+        price: '2.99'
+    },
+    {
+        name: 'CocaCola',
+        price: '2.99'
+    },
+    {
+        name: 'Pepsi',
+        price: '2.99'
+    },
+    {
+        name: 'Diet CocaCola',
+        price: '2.99'
+    },
+    {
+        name: 'Diet Pepsi',
+        price: '2.99'
+    },
+    {
+        name: 'Lemonade',
+        price: '1.99'
+    },
+    {
+        name: 'Strawberry Lemonade',
+        price: '3.99'
+    },
+    {
+        name: 'Water',
+        price: '0.99'
+    },
+    {
+        name: 'Coffee',
+        price: '3.99'
+    },
+    {
+        name: 'Latte',
+        price: '4.99'
+    },
+]
+
