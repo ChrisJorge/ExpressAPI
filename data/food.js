a55f53a6c87097e629156ac4360a01d5c8e2585f
Chris <Christopherjorge47@icloud.com>
2024-04-15 14:49:32 -0400

    Created food file

---
+++ data/food.js
@@ -0,0 +1,33 @@
+const food = [
+    {
+        name: 'pizza',
+        cost: '$12',
+        serves: '4 people',
+        sides: ['French Fries', 'Chips']
+    },
+
+    {
+        name: 'steak',
+        cost: '$30',
+        serves: '1 person',
+        sides: ['French Fries', 'Mashed Potatoes', 'Vegetables', 'Maccaroni and Cheese']
+    },
+    {
+        name: 'Chicken Parmesan',
+        cost: '$20',
+        serves: '1 person',
+        sides: ['French Fries', 'Mashed Potatoes','Sphagetti', 'Vegetables', 'Maccaroni and Cheese']
+    },
+    {
+        name: 'Hot Dog',
+        cost: '$6',
+        serves: '1 person',
+        sides: ['French Fries', 'Chips']
+    },
+    {
+        name: 'Sea Food Tower',
+        cost: '$60',
+        serves: '5 people',
+        sides: ['Mashed Potaoes']
+    }
+]
